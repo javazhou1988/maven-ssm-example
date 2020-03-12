3f11b2ac1519d4ed6a8971b754b355e1df033265
javazhou1988 <javazhou1988@163.com>
2020-03-12 12:32:12 +0800

    rest webservice 测试

--- manager-web/src/main/java/com/css/controller/UserController.java
+++ manager-web/src/main/java/com/css/controller/UserController.java
@@ -4,7 +4,9 @@ import com.css.model.User;
 import com.css.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @Author: javazhou
@@ -22,6 +24,30 @@ public class UserController {
     @RequestMapping("/addUser")
     public String addUser(User user) {
         userService.addUser(user);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/doGet/{userId}", method = RequestMethod.GET)
+    public String doGet(@PathVariable String userId) {
+        System.out.println("doGet..." + userId);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/doPost", method = RequestMethod.POST)
+    public String doPost(User user) {
+        System.out.println("doPost..." + user);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/doDelete", method = RequestMethod.DELETE)
+    public String doDelete(String userId) {
+        System.out.println("doDelete ..." + userId);
+        return "ok";
+    }
+
+    @RequestMapping(value = "/doPut", method = RequestMethod.PUT)
+    public String doPut(User user) {
+        System.out.println("doPut..." + user);
         return "ok";
     }
 }
